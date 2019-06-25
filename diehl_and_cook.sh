337f32d6a7bc244b3480264863dcfcfbad146cd5
Khanh N. Dang <khanhdang@users.noreply.github.com>
2019-06-25 15:58:00 +0900

    fix the path

--- diehl_and_cook.sh
+++ diehl_and_cook.sh
@@ -4,7 +4,7 @@ seed=${1:-0}
 n_neurons=${2:-100}
 n_train=${3:-60000}
 
-cd ../../../experiments/mnist/
+cd ./bindsnet_experiments/experiments/mnist/
 # activate the env
 
 python -u diehl_and_cook_2015.py --train --seed $seed --n_train $n_train  --n_test $n_test 
